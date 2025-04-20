cc0a4dedd71c2cabd1ecaea1cba8b9eabf547cb6
Plexe1 <84465447+Plexe1@users.noreply.github.com>
2025-04-20 10:28:05 +0200

    Versioning deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 cd /var/www/portfolio
 echo "Deploy started at $(date)" >> deploy.log
 git pull origin main
+VERSION=$(git rev-parse --short HEAD)
+sed -i "s/{{VERSION}}/$VERSION/g" /var/www/portfolio/index.html
 #npm install
 #npm run build
 sudo systemctl reload nginx
